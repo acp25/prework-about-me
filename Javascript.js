ac99eb2908e3705619b06da51e50701a2c2932fa
acp25 <60405505+acp25@users.noreply.github.com>
2021-05-24 11:29:49 -0400

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,19 @@
+document.getElementById("button1").addEventListener("click", function(){
+    document.getElementById("box").style.height = "200px";
+    document.getElementById("box").style.width = "200px";
+ });
+
+ document.getElementById("button2").addEventListener("click", function(){    
+    document.getElementById("box").style.backgroundColor = "blue";
+});
+
+document.getElementById("button3").addEventListener("click", function(){    
+    document.getElementById("box").style.backgroundColor = "#dff0d8";
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px";
+    document.getElementById("box").style.width = "150px";
+    document.getElementById("box").style.backgroundColor = "orange";
+});
